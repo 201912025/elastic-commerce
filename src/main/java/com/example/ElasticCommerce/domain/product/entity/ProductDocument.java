8e6309f8c7c13f4f013c0610984d35c5328e1a29
201912025 <michi2012@naver.com>
2025-05-30 16:59:21 +0900

    feat: productDocument 클래스 생성

---
+++ src/main/java/com/example/ElasticCommerce/domain/product/entity/ProductDocument.java
@@ -0,0 +1,53 @@
+package com.example.ElasticCommerce.domain.product.entity;
+
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.elasticsearch.annotations.*;
+
+import java.util.UUID;
+
+@Document(indexName = "products")
+@Setting(settingPath = "/elasticsearch/product-settings.json")
+@Getter
+public class ProductDocument {
+
+    @Id
+    private String id;
+
+    @MultiField(mainField = @Field(type = FieldType.Text, analyzer = "products_name_analyzer"),
+        otherFields = {
+            @InnerField(suffix = "auto_complete", type = FieldType.Search_As_You_Type, analyzer = "nori")
+        }
+    )
+    private String name;
+
+    @Field(type = FieldType.Text, analyzer = "products_description_analyzer")
+    private String description;
+
+    @Field(type = FieldType.Integer)
+    private Integer price;
+
+    @Field(type = FieldType.Double)
+    private double rating = 0.0;
+
+    @MultiField(mainField = @Field(type = FieldType.Text, analyzer = "products_category_analyzer"),
+            otherFields = {
+                    @InnerField(suffix = "raw", type = FieldType.Keyword)
+            }
+    )
+    private String category;
+
+    @Builder
+    public ProductDocument(String name, String description, Integer price, String category) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+
+    public void updateRating(double updateRating) {
+        this.rating = updateRating;
+    }
+}
